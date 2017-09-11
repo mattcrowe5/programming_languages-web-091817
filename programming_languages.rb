65baa809a136274b1f77d444d61fd26b5241c896
Matt Crowe <crowe.matthew5@gmail.com>
2017-09-11 14:58:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  hash ={}
+  languages.map do |style, data|
+    data.map do |language, attributes|
+      attributes.map do |type, descript|
+        if !hash.has_key?(language)
+        hash[language] = {type: descript}
+        end
+        if hash[language].has_key?(:style)
+          hash[language][:style].push(style)
+        elsif !hash[language].has_key?(:style)
+          hash[language][:style] = [style]
+        end
+    end
+  end
+end
+  hash
 end
